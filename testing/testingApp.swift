b15182e4f55c421cc95a3e35f69493769ca8e4d2
Riana-S <rianasantos808@gmail.com>
2025-04-16 15:26:42 -0700

    Initial Commit

---
+++ testing/testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingApp.swift
+//  testing
+//
+//  Created by Riana Santos on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
